170b6ba9cd782d93b839e22f44db43dda1696aec
avi-gross <avigross2018@gmail.com>
2026-05-13 08:53:05 +0300

    add func func main and the game runing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+MAX_ROUNDS = 5
+
 def computer_choose_rand_word():
     words = [
     "apple", "beach", "cloud", "drink", "ghost", "house", "lemon", "music", 
@@ -18,7 +20,7 @@ def encrypt_word(comp_choice : list):
     return ["X" for w in comp_choice]
 
 
-def user_input_letter():
+def user_input_letter(user_letters : list):
     while True:
         user_letter = input("Enter a letter: ").lower()
         if len(user_letter) > 1:
@@ -27,6 +29,9 @@ def user_input_letter():
         if not user_letter.isalpha():
             print("You can enter only a letter.")
             continue
+        if user_letter in user_letters:
+            print("You have already chosen this letter.")
+            continue
         return user_letter
 
 
@@ -34,3 +39,45 @@ def check_letter_in_word(user_letter : str, comp_choice : list):
     if user_letter in comp_choice:
         return True
     return False
+
+
+def insert_letter_to_word(user_letter : str, comp_choice : list, enc_word : list):
+    for i,l in enumerate(comp_choice):
+        if user_letter == l:
+            enc_word[i] = user_letter
+    return enc_word
+
+
+def check_if_completed_word(enc_word : list):
+    if "X" not in enc_word:
+        return True
+    return False
+
+
+def main():
+    comp_choice = computer_choose_rand_word()
+    enc_word = encrypt_word(comp_choice)
+    rounds_counter = 0
+    user_letters = []
+    while rounds_counter < MAX_ROUNDS:
+        print(MAX_ROUNDS - rounds_counter, "Rounds left")
+        print(enc_word)
+        print("Letter that you gusset", user_letters)
+        user_letter = user_input_letter(user_letters)
+        user_letters.append(user_letter)
+        if check_letter_in_word(user_letter, comp_choice):
+            enc_word = insert_letter_to_word(user_letter, comp_choice, enc_word)
+            print("The letter in the word.")
+        else:
+            rounds_counter += 1
+            print("The letter does not exist in the word, Try again")
+            continue
+        if check_if_completed_word(enc_word):
+            print(f"You Won. \nThe word is **{"".join(enc_word)}**")
+            break
+    print("No more rounds. You lose...")
+
+
+        
+
+main()
