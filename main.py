0ed0fbc7d70364336cbff0875aad65ae472717f5
avi-gross <avigross2018@gmail.com>
2026-05-13 15:18:31 +0300

    speread words the other file

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import random
+import words
 
 MAX_ROUNDS = 5
 
 def computer_choose_rand_word():
-    words = [
-    "apple", "beach", "cloud", "drink", "ghost", "house", "lemon", "music", 
-    "pizza", "smile", "tiger", "water", "balloon", "camera", "dancing", 
-    "explore", "friends", "guitar", "journey", "kitchen", "mystery", 
-    "orange", "puzzle", "rocket", "sunlight", "whisper", "adventure", 
-    "astronaut", "championship", "diamond", "elevator", "flamingo", 
-    "hospital", "labyrinth", "mountain", "nightmare", "orchestra", 
-    "satellite", "umbrella", "volcano", "jazz", "rhythm", "oxygen", 
-    "queue", "sphinx", "whizz", "zigzag", "awkward"
-    ]
-    return list(random.choice(words))
+    return list(random.choice(words.words))
 
 
 def encrypt_word(comp_choice : list):
-    return ["X" for w in comp_choice]
+    return ["#" for w in comp_choice]
 
 
 def user_input_letter(user_letters : list):
@@ -49,23 +40,27 @@ def insert_letter_to_word(user_letter : str, comp_choice : list, enc_word : list
 
 
 def check_if_completed_word(enc_word : list):
-    if "X" not in enc_word:
+    if "#" not in enc_word:
         return True
     return False
 
 
 def main():
-    print("=== HangMan ===")
+    print("=== HangMan ===\n")
     comp_choice = computer_choose_rand_word()
     enc_word = encrypt_word(comp_choice)
     rounds_counter = 0
     user_letters = []
+    
     while rounds_counter < MAX_ROUNDS:
-        print(MAX_ROUNDS - rounds_counter, "Rounds left")
         print("".join(enc_word))
+        print("==========")
+        print(MAX_ROUNDS - rounds_counter, "Rounds left")
         print("Letter that you gusset", user_letters)
+
         user_letter = user_input_letter(user_letters)
         user_letters.append(user_letter)
+
         if check_letter_in_word(user_letter, comp_choice):
             enc_word = insert_letter_to_word(user_letter, comp_choice, enc_word)
             print("The letter in the word.")
@@ -73,6 +68,7 @@ def main():
             rounds_counter += 1
             print("The letter does not exist in the word, Try again")
             continue
+
         if check_if_completed_word(enc_word):
             print(f"You Won. \nThe word is **{"".join(enc_word)}**")
             return
